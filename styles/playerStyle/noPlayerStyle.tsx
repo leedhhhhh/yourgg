d571109394c7f17a882f9a8b949d4a9fe5b07583
이동형 <leedhhhhh@gmail.com>
2022-10-16 22:31:15 +0900

    소환사가 없는 경우

---
+++ styles/playerStyle/noPlayerStyle.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+export const Svg = styled.svg`
+  margin-top: 300px;
+  margin-left: 200px;
+  margin-bottom: 30px;
+`;
+
+export const Text = styled.div`
+  font-size: 15px;
+  font-family: "Source Sans Pro", sans-serif;
+  text-align: center;
+`;
+
+export const btnContainer = styled.div`
+  width: 100px;
+  height: 30px;
+  background-color: #eeeeee;
+  text-align: center;
+  padding-top: 3px;
+  margin-top: 20px;
+  margin-left: 170px;
+  border-radius: 10px;
+  cursor: pointer;
+`;
