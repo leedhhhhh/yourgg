060d432f627a87ea478fbf4b6ad8df67ae82afb9
이동형 <leedhhhhh@gmail.com>
2022-10-16 22:31:35 +0900

    타이틀추가

--- pages/players/[name].tsx
+++ pages/players/[name].tsx
@@ -2,11 +2,13 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { playerNameState } from "../api/atom";
 import { useQuery } from "react-query";
 import { useEffect, useState } from "react";
-import Normal from "../games/Normal";
-import SoloRank from "../games/SoloRank";
+import Normal, { ISoloRankProps } from "../games/Normal";
+import SoloRank, { getSoloRankInfo } from "../games/SoloRank";
 import FreeRank from "../games/FreeRank";
-import * as X from "../../styles/playerStyle";
+import * as X from "../../styles/playerStyle/playerStyle";
 import NoSsr from "../NoSsr";
+import NoPlayer from "../noPlayer/NoPlayer";
+import Link from "next/link";
 
 export default function Players() {
   const playerName = useRecoilValue(playerNameState);
@@ -16,23 +18,37 @@ export default function Players() {
     setGame(event.target.value);
   };
 
+  const { data: soloRankData, isLoading } = useQuery<ISoloRankProps>(
+    "Error",
+    () => getSoloRankInfo(playerName)
+  );
+
   return (
-    <X.Container>
-      <X.PlayerName>
-        <NoSsr>{playerName}</NoSsr>
-      </X.PlayerName>
-      <X.SelectRank>
-        <X.SelectBox defaultValue="SoloRank" onChange={onHandle}>
-          <option value="Normal">Normal</option>
-          <option value="SoloRank">SoloRank</option>
-          <option value="FreeRank">FreeRank</option>
-        </X.SelectBox>
-      </X.SelectRank>
-      <div>
-        {game === "Normal" && <Normal />}
-        {game === "SoloRank" && <SoloRank />}
-        {game === "FreeRank" && <FreeRank />}
-      </div>
-    </X.Container>
+    <>
+      {soloRankData?.id === "Exception" ? (
+        <NoPlayer />
+      ) : (
+        <X.Container>
+          <Link href="/">
+            <X.Title>YOURGG</X.Title>
+          </Link>
+          <X.PlayerName>
+            <NoSsr>{playerName}</NoSsr>
+          </X.PlayerName>
+          <X.SelectRank>
+            <X.SelectBox defaultValue="SoloRank" onChange={onHandle}>
+              <option value="Normal">Normal</option>
+              <option value="SoloRank">SoloRank</option>
+              <option value="FreeRank">FreeRank</option>
+            </X.SelectBox>
+          </X.SelectRank>
+          <div>
+            {game === "Normal" && <Normal />}
+            {game === "SoloRank" && <SoloRank />}
+            {game === "FreeRank" && <FreeRank />}
+          </div>
+        </X.Container>
+      )}
+    </>
   );
 }
